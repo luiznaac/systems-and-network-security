66f0c37e181e7fb11591eee8ff9a20747b07ff0f
luiznaac <luizagnern@hotmail.com>
2020-03-18 21:41:21 -0300

    Fixing considered alphabet

--- src/cipher/symmetric/CesarCipher.py
+++ src/cipher/symmetric/CesarCipher.py
@@ -4,70 +4,92 @@ from CipherInterface import CipherInterface
 class CesarCipher(CipherInterface):
 
     def cipher(self, key, clear_text):
-        return self.getCipherText(key, clear_text)
+        char_array = self.initializeCharArray()
+        return self.getCipherText(key, clear_text, char_array)
 
     def decipher(self, key, cipher_text):
-        return self.getClearText(key, cipher_text)
+        char_array = self.initializeCharArray()
+        return self.getClearText(key, cipher_text, char_array)
 
-    def getCipherText(self, key, clear_text):
+    def initializeCharArray(self):
+        char_array = self.getCharsInRange(ord('a'), ord('z'))
+        char_array += self.getCharsInRange(ord('A'), ord('Z'))
+        char_array += self.getCharsInRange(ord('0'), ord('9'))
+
+        return char_array
+
+    def getCharsInRange(self, initial_value, final_value):
+        chars = []
+
+        for char_value in range(initial_value, final_value + 1):
+            chars.append(chr(char_value))
+
+        return chars
+
+    def getCipherText(self, key, clear_text, char_array):
         cipher_text = ''
 
         for char in clear_text:
-            cipher_text = '{}{}'.format(cipher_text, self.getCipherChar(key, char))
+            cipher_text = '{}{}'.format(cipher_text, self.getCipherChar(key, char, char_array))
 
         return cipher_text
 
-    def getCipherChar(self, key, char):
+    def getCipherChar(self, key, char, char_array):
         if not self.shouldApplyKeyToChar(char):
             return char
 
-        char_value = ord(char)
+        clear_char_index = char_array.index(char)
+        cipher_char_index = self.getCipherCharIndex(
+            key,
+            clear_char_index,
+            char_array.__len__()
+        )
 
-        return chr(self.getCipherCharValue(key, char_value))
+        return char_array[cipher_char_index]
 
-    def getCipherCharValue(self, key, char_value):
-        base_value = 96
-        if self.isCapitalLetter(char_value):
-            base_value = 64
+    def getCipherCharIndex(self, key, clear_char_index, char_array_length):
+        key = int(key) % char_array_length
+        char_array_length -= 1
+        cipher_char_index = clear_char_index + key
 
-        cipher_value = char_value + int(key) - base_value
+        if cipher_char_index > char_array_length:
+            return cipher_char_index - char_array_length
 
-        if cipher_value > 26:
-            return cipher_value - 26 + base_value
+        return cipher_char_index
 
-        return cipher_value + base_value
-
-    def getClearText(self, key, cipher_text):
+    def getClearText(self, key, cipher_text, char_array):
         clear_text = ''
 
         for char in cipher_text:
-            clear_text = '{}{}'.format(clear_text, self.getClearChar(key, char))
+            clear_text = '{}{}'.format(clear_text, self.getClearChar(key, char, char_array))
 
         return clear_text
 
-    def getClearChar(self, key, char):
+    def getClearChar(self, key, char, char_array):
         if not self.shouldApplyKeyToChar(char):
             return char
 
-        char_value = ord(char)
+        cipher_char_index = char_array.index(char)
+        clear_char_index = self.getClearCharIndex(
+            key,
+            cipher_char_index,
+            char_array.__len__()
+        )
 
-        return chr(self.getClearCharValue(key, char_value))
+        return char_array[clear_char_index]
 
-    def getClearCharValue(self, key, char_value):
-        base_value = 96
-        if self.isCapitalLetter(char_value):
-            base_value = 64
+    def getClearCharIndex(self, key, cipher_char_index, char_array_length):
+        key = int(key) % char_array_length
+        char_array_length -= 1
+        clear_char_index = cipher_char_index - key
 
-        clear_value = char_value - int(key) - base_value
+        if clear_char_index < 0:
+            return char_array_length + clear_char_index
 
-        if clear_value < 1:
-            return 26 + clear_value + base_value
-
-        return clear_value + base_value
+        return clear_char_index
 
     def shouldApplyKeyToChar(self, char):
         char_value = ord(char)
-        return (65 <= char_value <= 90) or (97 <= char_value <= 122)
-
-    def isCapitalLetter(self, char_value):
-        return 65 <= char_value <= 90
+        return (ord('a') <= char_value <= ord('z')) or \
+               (ord('A') <= char_value <= ord('Z')) or \
+               (ord('0') <= char_value <= ord('9'))
